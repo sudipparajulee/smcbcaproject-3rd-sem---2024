0317e9856ccd43e475cd3fffa8c3a69b47a5d881
Sudip Parajuli <sudip.parajulee@gmail.com>
2026-02-10 08:28:53 +0545

    add initial dashboard layout with navigation

---
+++ admin/dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex">
+        <div class="w-52 h-screen bg-gray-100">
+            <h1 class="text-center mt-5 font-bold text-2xl">LOGO</h1>
+            <nav class="mt-4">
+                <a href="" class="block text-lg p-3 hover:bg-gray-200 border-b">Dashboard</a>
+                <a href="" class="block text-lg p-3 hover:bg-gray-200 border-b">Categories</a>
+                <a href="" class="block text-lg p-3 hover:bg-gray-200 border-b">News</a>
+                <a href="" class="block text-lg p-3 hover:bg-gray-200 border-b">Logout</a>
+            </nav>
+        </div>
+        <div class="p-4 flex-1">
+            <div>
+                <h2 class="font-bold text-3xl">Dashboard</h2>
+                <hr class="h-1 bg-red-600">
+                <p>Rest of the content here</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
