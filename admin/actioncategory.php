527f2918333b1f78bbd15e1e76c153127271f2ad
Sudip Parajuli <sudip.parajulee@gmail.com>
2026-02-16 08:24:45 +0545

    Data Entry Successful

--- admin/actioncategory.php
+++ admin/actioncategory.php
@@ -1,17 +1,23 @@
 <?php 
 $name = $_POST['name'];
 $priority = $_POST['priority'];
-echo $priority;
-echo $name;
-//create connection 
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'smc_bca3rd2024';
 
-$conn = new mysqli($server, $username, $password, $database);
+//get connection 
+include 'includes/dbconnection.php';
 //setup query 
 $qry = "INSERT INTO categories (name, priority) VALUES ('$name', $priority)";
 
 //run query 
+$result = $conn->query($qry);
+if($result)
+{
+    echo "Category added successfully";
+}
+else
+{
+    echo "Error: " . $conn->error;
+}
+
+//close connection
+include 'includes/closeconnection.php';
 ?>
---
+++ admin/includes/dbconnection.php
@@ -0,0 +1,8 @@
+<?php
+//create connection 
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'smc_bca3rd2024';
+
+$conn = new mysqli($server, $username, $password, $database);
